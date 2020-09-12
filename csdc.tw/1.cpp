8354e3bc39205c78f66966afaa8aa14099ca5c0d
wh00am1 <xthehacker666@gmail.com>
2020-09-12 10:08:10 +0800

    update csdc

---
+++ csdc.tw/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	return 0;
+}
