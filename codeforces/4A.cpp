9638bb193996d84357d14fde50bee6c7d606b4f9
Ethan Lai <xthehacker666@gmail.com>
2022-12-14 17:46:37 +0800

    Create 4A.cpp

---
+++ codeforces/4A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#define ll long long
+#define endl " \n"
+#define AkagiMyWife ios_base::sync_with_stdio(0); cin.tie(0);
+using namespace std;
+
+const int INF = 0x3f3f3f3f;
+
+int main(int argc, char const *argv[]){ AkagiMyWife
+    int n;
+    cin >> n;
+    cout << ((n%2==0 && n>2) ? "YES":"NO") << endl[1];
+	return 0;
+}
