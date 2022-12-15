7a08c6aaa6670984bf7057dfd1edf247eb7dd264
Ethan Lai <xthehacker666@gmail.com>
2022-12-15 19:00:15 +0800

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -4,21 +4,18 @@
 #define ll long long
 #define ld long double
 #define endl " \n"
-#define AkagiMyWife ios_base::sync_with_stdio(0); cin.tie(0);
-#define FOR(a, b) for(int (a)=0; (a)<(b); (a)+=1)
-#define EACH(u, v) for(auto &u : v)
+#define FOR(a, b) for(ll (a)=0; (a)<(ll)(b); ++(a))
+#define EACH(u, v) for(auto& u : v)
 
 using namespace std;
 
 const int INF = 0x3f3f3f3f;
 
-int main(int argc, char const *argv[]){ 
-	AkagiMyWife
-	vector<int> V;
-	FOR(i, 10){
-		V.push_back(i);
-	}		
-	EACH(I, V)
-		cout << I << endl[I==*(V.end())];
+void AkagiMyWife(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+}
+int main(int argc, char const *argv[]){
+	AkagiMyWife();	
 	return 0;
 }
