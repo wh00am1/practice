63859489e961db1758ffb5b00108dc01ed12f835
Ethan Lai <xthehacker666@gmail.com>
2022-12-15 19:00:03 +0800

    Update default.cpp

--- default.cpp
+++ default.cpp
@@ -4,15 +4,18 @@
 #define ll long long
 #define ld long double
 #define endl " \n"
-#define AkagiMyWife ios_base::sync_with_stdio(0); cin.tie(0);
-#define FOR(a, b) for(int (a)=0; (a)<(b); (a)+=1)
+#define FOR(a, b) for(ll (a)=0; (a)<(ll)(b); ++(a))
 #define EACH(u, v) for(auto& u : v)
 
 using namespace std;
 
 const int INF = 0x3f3f3f3f;
 
-int main(int argc, char const *argv[]){ 
-	AkagiMyWife	
+void AkagiMyWife(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+}
+int main(int argc, char const *argv[]){
+	AkagiMyWife();	
 	return 0;
 }
