846e2e5b17dfb6bd0dd7fe72bb50de7cb6f37cb5
Ethan Lai <xthehacker666@gmail.com>
2023-01-18 18:38:16 +0800

    Update 1352A.cpp

--- codeforces/1352A.cpp
+++ codeforces/1352A.cpp
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-#define ll long long
-#define ld long double
-#define FOR(a, b) for(ll (a)=0; (a)<(ll)(b); ++(a))
-#define EACH(u, v) for(auto& u : v)
-
 using namespace std;
-
-const int INF = 0x3f3f3f3f;
-
-void AkagiMyWife(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-}
-int t, n;
-vector<int> v={10000, 1000, 100, 10, 1};
+#define ll long long
+#define endl '\n'
+#define AkagiMyWife ios_base::sync_with_stdio(0); cin.tie(0);
+int a[4]={10000, 1000, 100, 10};
 void solve(){
+	int n;
 	cin >> n;
-	cout << '\n';
+	vector<int> ans;
+	for(int i=0; i<4; ++i){
+		if(n>a[i]){
+			ans.emplace_back((n/a[i])*a[i]);
+			n-=(n/a[i])*a[i];
+		}
+	}
+	if(n>0)
+		ans.emplace_back(n);
+
+	cout << ans.size() << endl;
+	for(int i : ans)
+		cout << i << endl;
 }
 int main(int argc, char const *argv[]){
-	AkagiMyWife();
+	AkagiMyWife
+	int t;
 	cin >> t;
-	while(t--)
-			solve();	
+	while(t--){
+		solve();
+	}
 	return 0;
 }
