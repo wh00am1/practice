c8327f1c114f34423ed2b0099ee5a2ad9c17a8cd
Ethan Lai <xthehacker666@gmail.com>
2023-01-28 10:56:20 +0800

    Create longest_flight_route.cpp

---
+++ cses/longest_flight_route.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define AkagiMyWife ios_base::sync_with_stdio(0); cin.tie(0);
+
+int main(int argc, char const *argv[]){
+	AkagiMyWife
+	
+	return 0;
+}
